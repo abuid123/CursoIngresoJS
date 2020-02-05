15100c818d7815d8751afbb1a6f4024405471156
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-05 00:03:31 -0300

    Ejercicio 7
    Tomamos los numeros por un ID, los transformamos a enteros, hacemos la cuenta y lo mostramos en un alert

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,71 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var suma;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero = parseInt(primerNumero);
+
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
+
+	suma = primerNumero + segundoNumero;
+
+	alert(suma);
+
+
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero = parseInt(primerNumero);
+
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero - segundoNumero;
+
+	alert(resultado);
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero = parseInt(primerNumero);
+
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero * segundoNumero;
+
+	alert(resultado);
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero = parseInt(primerNumero);
+
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero / segundoNumero;
+
+	alert(resultado);
 }
 
