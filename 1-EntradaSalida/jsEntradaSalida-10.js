206a68c88f0608b966b7057af6c1b993c554b42a
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-05 00:14:01 -0300

    Ejercicio 10
    Tomar importe por el ID, transformarlo a entero, luego mostrar el importe con un descuento de 25%

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+	var descuento;
+	var resultadoFinal;
+
+	importe = document.getElementById('importe').value;
+	importe = parseInt(importe);
+
+	descuento = 25/100*importe;
+
+	resultadoFinal = importe - descuento;
+
+	document.getElementById('resultado').value = resultadoFinal;
 }
