0081b3af3b4b91f60060c861fb98c61a5993bc55
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-05 00:12:31 -0300

    Ejercicio 9
    Tomar importe por ID transformarlo a entero, y luego mostrar el importe con un 10% de aumento

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importe;
+	var porcentaje;
 
+	importe=document.getElementById('sueldo').value;
+	importe=parseInt(importe);
+
+	porcentaje = 10/100*importe;
+
+	importe = importe + porcentaje;
+
+	document.getElementById('resultado').value=importe;
 	
 }
