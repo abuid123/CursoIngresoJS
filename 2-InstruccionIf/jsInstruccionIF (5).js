4ac71834b11c49d1afbc570a2f6946605cf15e63
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 06:43:20 -0300

    Ejercicio 5
    Usamos el OR || para mandar un mensaje solo si la persona no es adolescente

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById('edad').value;
+//parseo la variable edad
+edad = parseInt(edad);
+
+//pongo la condicion
+if(edad < 13 || edad > 17){
+
+	alert("No sos un adolescente.");
+}
 
 }//FIN DE LA FUNCIÓN
