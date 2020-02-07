5105a81327b73006100ef759f7edb5cd1f14f1a1
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 06:39:52 -0300

    Ejercicio 4
    IF con 2 condiciones (&&)

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById('edad').value;
+//parseo la variable edad
+edad = parseInt(edad);
+
+if(edad <= 17 && edad >= 13){
+
+	alert("Sos un adolescente.");
+
+}else{
+
+	alert("No sos adolescente.");
+}
 
 
 }//FIN DE LA FUNCIÓN
