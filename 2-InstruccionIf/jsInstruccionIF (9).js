5d8b2bd5ec3dd239727b6248c5fdc2c78fb34f9c
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 07:07:26 -0300

    Ejercicio 9

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,9 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	var numeroRandom = Math.floor(Math.random()*(11-0))+0;
+
+	alert("Numero aleatorio entre 1 y 10: " + numeroRandom);
 
 }//FIN DE LA FUNCIÓN
