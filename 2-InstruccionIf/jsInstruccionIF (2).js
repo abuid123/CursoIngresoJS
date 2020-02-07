5824c80d18ae60392fdfba407706b7f1f86a3ed7
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 06:29:32 -0300

    Ejercicio 2

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById('edad').value;
 
+//parseo el valor de edad
+edad = parseInt(edad);
+
+if(edad >= 18){
+	alert("Sos mayor de edad");
+}
 
 }//FIN DE LA FUNCIÓN
