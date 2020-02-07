d10756d83cafe1eb16f3c00566ad4b7321080a2b
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 06:46:53 -0300

    Ejercicio 6
    Lo resolví a través de un else if

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById('edad').value;
+//parseo la variable edad
+edad = parseInt(edad);
+
+if(edad <= 17 && edad >= 13){
+
+	alert("Sos un adolescente.");
+
+}else if(edad >=18){
+
+	alert("Sos mayor de edad.");
+}else{
+	alert("Sos un niño.");
+}
 
 
 
