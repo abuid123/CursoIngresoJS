ad6f6071541e6cdbd49e69cebd3cb90c5f1bec70
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 06:32:12 -0300

    Ejercicio 3
    Lo mismo que el ejercicio 1 y 2, pero en este caso le agregamos un ELSE para poner que de lo contrario a ser mayor de edad mande un msj que es menor de edad

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById('edad').value;
+//parseo la variable edad
+edad = parseInt(edad);
+
+if(edad >= 18){
+
+	alert("Sos mayor de edad.");
+
+}else{
+
+	alert("Sos menor de edad.");
+}
 
 }//FIN DE LA FUNCIÓN
