271b53c4b71c61309beed1e9251f7a197a747964
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 06:51:00 -0300

    Ejercicio 7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
 
+edad = document.getElementById('edad').value;
+//parseo la variable edad
+edad = parseInt(edad);
+
+estadoCivil = document.getElementById('estadoCivil').value;
+
+if(edad < 18 && estadoCivil != "Soltero"){
+
+	alert('Es muy pequeño para NO ser soltero.');
+
+}
 	
 
 
