7af4ca059f4f4b65f960280e5469bb79c3fb0c41
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-07 06:54:30 -0300

    Ejercicio 8

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
+
+edad = document.getElementById('edad').value;
+//parseo la variable edad
+edad = parseInt(edad);
+
+estadoCivil = document.getElementById('estadoCivil').value;
+
+if(edad >= 18 && estadoCivil == "Soltero"){
+
+	alert('Es soltero y no es menor.');
 
 	
-
+}
 
 }//FIN DE LA FUNCIÓN
