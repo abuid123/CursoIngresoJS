e1af98859ffe861daa9a8850c3dfd7af6613df15
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-08 19:50:32 -0300

    Ejercicio 5

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,9 +1,27 @@
 function mostrar()
 {
-//tomo la edad  
 var laHora = document.getElementById('hora').value;
+var laHora = parseInt(laHora);
+switch(laHora){
+	case 7:
+		alert("Es de mañana.");
+		break;
+	case 8:
+		alert("Es de mañana.");
+		break;
+	case 9:
+		alert("Es de mañana.");
+		break;
+	case 10:
+		alert("Es de mañana.");
+		break;
+	case 11:
+		alert("Es de mañana.");
+		break;
+	default:
+		alert("Ingrese una hora entre las 7 y las 11.");				
+}
 
-//alert (laHora);
 	
 	
 
