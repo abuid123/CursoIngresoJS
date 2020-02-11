a615ba09b5e2b902688f1278e48a4b0f67936ccc
abuid123 <60628876+abuid123@users.noreply.github.com>
2020-02-10 22:05:51 -0300

    Ejercicio 10 v2

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom = Math.floor(Math.random()*(11-0))+0;
+
+	if(numeroRandom < 4){
+		alert("La nota es: "+numeroRandom +" Vamos, la proxima se puede");
+	}else
+	{
+		if(numeroRandom > 3 && numeroRandom < 9){
+			alert("La nota es: "+numeroRandom +" APROBO");
+		}else
+		{
+			alert("La nota es: "+numeroRandom +" Excelente.");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
